1871d60351329d8e1c43dba09aac40005cdf2d68
Sean Booker Jr <seanbookerjunior@gmail.com>
2022-08-01 09:57:15 -0400

    nothing again

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -6,10 +6,10 @@ function Login() {
   const navigate = useNavigate();
   return (
     <div className="login">
-      <h1 className="logintitle">Chef It</h1>
       <div className="loginform">
-        <h1 className="logintext">Your go to place to hire personal chefs</h1>
-        <h1 className="logintext">Why cook when you can chef it!</h1>
+      <h1 className="logintitle">Chef It</h1>
+        <h1>Your go to place to hire personal chefs</h1>
+        <h1>Why cook when you can chef it!</h1>
         <button type="button" onClick={() => { navigate('/user'); }}>Login as User </button>
         <button type="button" onClick={() => { navigate('/chef'); }}>Login as Chef</button>
         <CreateAccountModal />
